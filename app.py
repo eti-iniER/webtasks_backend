f4ae8ee8c51a4170543a67de23ca461070fb084e
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-03-08 14:31:40 +0100

    Flask tests

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def print_name(name):
     return f"<h1>Hello, {name} </h1>"
 
 
+@app.route("/dance")
+def do_dance():
+    return "<h1>Bad request</h1>", 400
+
+
 if __name__ == "__main__":
     app.run(debug=True)
