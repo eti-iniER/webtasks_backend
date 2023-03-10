96a014059240d840d21119c7239b4efcdad1beff
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-03-10 17:12:47 +0100

    Reset

--- app.py
+++ app.py
@@ -1,18 +1,1 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-people = [
-    {"name": "John McAdams",
-     "age": 45,
-     "favourite_food": "Scrambled Eggs"}
-]
-
-
-@app.route("/api")
-def get_people():
-    return jsonify(people)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from flask import Flask
